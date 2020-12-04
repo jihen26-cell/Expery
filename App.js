eee208de2d60dab98a334d3cf04110b16bfe3039
jihen26-cell <55513074+jihen26-cell@users.noreply.github.com>
2020-12-04 22:15:04 +0000

    Add files via upload

---
+++ App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Sidebar from "./Components/Sidebar";
+
+function App()
+{
+  return(
+    <div className="App">
+      </div>
+  );
+}
+export default App;
---
+++ SidebarData.js
@@ -0,0 +1,28 @@
+import React from "react"
+import MeetingIcon from "@material-ui/icons/Meeting";
+import CalendarToday from "@material-ui/icons/CalendarToday";
+import Payement from "@material-ui/icons/Payement";
+import Settings from "@material-ui/icons/Settings";
+export const SidebarData = [
+    {
+    title:"Meeting",
+    icon:<MeetingIcon/>,
+    link:"/Meeting"
+},
+{
+    title:"Calendar",
+    icon:<CalendarToday/>,
+    link:"/Calendar"
+},
+{
+    title:"Payement",
+    icon:<MeetingIcon/>,
+    link:"/Payement"
+},
+{
+    title:"Settings",
+    icon:<MeetingIcon/>,
+    link:"/Settings"
+},
+]
+
